7be38bfc877a79ad2f0060820d48a740119aeb93
Chris Yeung <jackknife29jk@gmail.com>
2023-08-07 16:48:35 -0300

    commit for07-aug-2023
    commit for07-aug-2023

---
+++ solutionsMe/helloworld03.js
@@ -0,0 +1,14 @@
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB',
+    'Python',
+    'Rust'
+]
+
+webTechs.splice(0, webTechs.length)
+console.log(webTechs)
